880508a34e049b78171738bb871d02793a4ad5f1
unknown <hein0040@WJ316-20.ottawa.ad.algonquincollege.com>
2015-01-24 03:31:54 -0500

    Added princess.

---
+++ Jasons Hero/Assets/Scripts/AI/Princess.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Princess : MonoBehaviour {
+
+	CharacterController m_Controller;
+	const float SPEED = 75.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		m_Controller = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (transform.position.x > 1.0f)
+		{
+			m_Controller.SimpleMove(Vector3.left * SPEED * Time.deltaTime);
+		}
+		else if (transform.position.x < -1.0f)
+		{
+			m_Controller.SimpleMove(Vector3.right * SPEED * Time.deltaTime);
+		}
+		else
+		{
+			m_Controller.SimpleMove(Vector3.zero);
+		}
+	}
+}
