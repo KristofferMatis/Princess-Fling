e1f4475eff33d1dd220cc8300ccdf4fcb1790ec2
unknown <hein0040@WJ316-20.ottawa.ad.algonquincollege.com>
2015-01-23 23:16:48 -0500

    Added basic camera functions.

---
+++ Jasons Hero/Assets/Scripts/Camera.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	//Followed enemy
+	public Transform m_FollowedTransform;
+	
+	//Update
+	void Update ()
+	{
+		updatePosition ();
+	}
+
+	//Lerps the camera to the followed enemy
+	void updatePosition ()
+	{
+		Vector3 pos = Vector3.Lerp (transform.position, m_FollowedTransform.position, 0.1f);
+		setPosition (pos);
+	}
+
+	//Sets the cameras position
+	public void setPosition(Vector3 pos)
+	{
+		transform.position = new Vector3 (pos.x, pos.y, -10.0f);
+	}
+
+	//Set a new transform to follow
+	public void setFollowedTransform (Transform followed)
+	{
+		m_FollowedTransform = followed;
+	}
+}
