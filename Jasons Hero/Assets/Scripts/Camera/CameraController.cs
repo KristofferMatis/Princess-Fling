533c10d7fd902b54cd98bfdf60506f6ef76edc83
unknown <hein0040@WJ316-12.ottawa.ad.algonquincollege.com>
2015-01-24 17:03:16 -0500

    Added new camera and basic respawning.

--- Jasons Hero/Assets/Scripts/Camera/CameraController.cs
+++ Jasons Hero/Assets/Scripts/Camera/CameraController.cs
@@ -1,8 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour
-{
+public class CameraController : MonoBehaviour {
+
+	//Followed enemy
+	public Transform m_FollowedTransform;
+
+	void Start ()
+	{
+		setPosition(m_FollowedTransform.position);
+	}
+
+	//Update
+	void Update ()
+	{
+		updatePosition ();
+	}
+
+	//Lerps the camera to the followed enemy
+	void updatePosition ()
+	{
+		Vector3 pos = Vector3.Lerp (transform.position, m_FollowedTransform.position, 0.1f);
+		setPosition (pos);
+	}
+
 	//Sets the cameras position
 	public void setPosition(Vector3 pos)
 	{
---
+++ Jasons Hero/Assets/Scripts/Misc/KillZone.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillZone : MonoBehaviour {
+
+	void OnTriggerEnter (Collider other)
+	{
+		Respawner respawner = other.GetComponent<Respawner>();
+		if (respawner != null)
+		{
+			respawner.Kill();
+		}
+	}
+}
