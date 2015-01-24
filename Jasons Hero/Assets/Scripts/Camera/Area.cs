33fbeff447eaff975346f976978190930040e6e2
unknown <hein0040@WJ316-12.ottawa.ad.algonquincollege.com>
2015-01-24 15:12:19 -0500

    Added basic respawning and camera following for areas.

---
+++ Jasons Hero/Assets/Scripts/Camera/Area.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Area : MonoBehaviour
+{
+	public CameraController m_Camera;
+	public Transform m_PlayerOneRespawn;
+	public Transform m_PlayerTwoRespawn;
+
+	//When this area is exited
+	void OnTriggerExit (Collider collider)
+	{
+		if (collider.gameObject.name == "Princess")
+		{
+			m_Camera.setPosition (transform.position);
+		}
+		else if (collider.gameObject.name == "PlayerOne")
+		{
+			collider.transform.position = m_PlayerOneRespawn.position;
+		}
+		else if (collider.gameObject.name == "PlayerTwo")
+		{
+			collider.transform.position = m_PlayerTwoRespawn.position;
+		}
+	}
+}
--- Jasons Hero/Assets/Scripts/Camera/CameraController.cs
+++ Jasons Hero/Assets/Scripts/Camera/CameraController.cs
@@ -1,34 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour {
-
-	//Followed enemy
-	public Transform m_FollowedTransform;
-	
-	//Update
-	void Update ()
-	{
-		updatePosition ();
-	}
-
-	//Lerps the camera to the followed enemy
-	void updatePosition ()
-	{
-		Vector3 pos = Vector3.Lerp (transform.position, m_FollowedTransform.position, 0.1f);
-		setPosition (pos);
-	}
-
+public class CameraController : MonoBehaviour
+{
 	//Sets the cameras position
 	public void setPosition(Vector3 pos)
 	{
 		transform.position = new Vector3 (pos.x, pos.y, -10.0f);
-	}
-
-	//Set a new transform to follow
-	public void setFollowedTransform (Transform followed)
-	{
-		m_FollowedTransform = followed;
-		setPosition(followed.position);
 	}
 }
