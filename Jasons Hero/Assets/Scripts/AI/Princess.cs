b30197c3aa6b006fbe1fe457b14a0df57788fd5e
unknown <hein0040@WJ316-12.ottawa.ad.algonquincollege.com>
2015-01-24 21:51:40 -0500

    Changed princess speed.

--- Jasons Hero/Assets/Scripts/AI/Princess.cs
+++ Jasons Hero/Assets/Scripts/AI/Princess.cs
@@ -5,7 +5,7 @@ public class Princess : Throwable
 {
 	Thrower LastThrower;
 
-	const float SPEED = 250.0f;
+	const float SPEED = 500.0f;
 	public override Thrower BeingCarriedBy
 	{
 		get { return BeingCarriedBy; }
