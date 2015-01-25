3c472e6d12060f1a1bc3f15f746ef0d62efa19ed
unknown <hein0040@WJ316-12.ottawa.ad.algonquincollege.com>
2015-01-25 05:47:47 -0500

    Added tiles, updated sound files, and added ROUND images.

--- Jasons Hero/Assets/Scripts/Misc/Win.cs
+++ Jasons Hero/Assets/Scripts/Misc/Win.cs
@@ -11,12 +11,53 @@ public class Win : MonoBehaviour
 
 	int m_Points = 0;
 
+	public Texture[] m_Round1Textures;
+	public Texture[] m_Round2Textures;
+	public Texture[] m_Round3Textures;
+	float m_Timer = -1.0f;
+	int index = 0;
+
 	void Start ()
 	{
 		m_Texture = GetComponentInParent<GUITexture>();
 		m_Texture.enabled = false;
 		m_AllThrowables = Object.FindObjectsOfType<Throwable> ();
 		m_Audio = GetComponent<AudioSource>();
+	}
+
+	void Update ()
+	{
+		if (m_Timer > -1.0f)
+		{
+			if (m_Timer > 0.0f)
+			{
+				m_Timer -= Time.deltaTime;
+			}
+			else if (index < m_Round1Textures.Length - 1)
+			{
+				m_Timer = 1.0f;
+				index++;
+
+				if (m_Points == 1)
+				{
+					m_Texture.texture = m_Round1Textures[index];
+				}
+				else if (m_Points == 2)
+				{
+					m_Texture.texture = m_Round2Textures[index];
+				}
+				else
+				{
+					m_Texture.texture = m_Round3Textures[index];
+				}
+			}
+			else
+			{
+				index = 0;
+				m_Timer = -1.0f;
+				m_Texture.enabled = false;
+			}
+		}
 	}
 
 	void OnTriggerEnter (Collider other)
@@ -26,20 +67,35 @@ public class Win : MonoBehaviour
 			//Play sound
 			m_Audio.PlayOneShot(m_Audio.clip);
 
-			m_Points++;
-
 			foreach (Throwable throwable in m_AllThrowables)
 			{
 				throwable.Reset();
 			}
 
+			index = 0;
+
 			m_Points++;
 			if (m_Points >= 3)
 			{
 				m_Texture.texture = m_WinTexture;
-				m_Texture.enabled = true;
-				return;
 			}
+			else
+			{
+				if (m_Points == 1)
+				{
+					m_Texture.texture = m_Round1Textures[index];
+				}
+				else if (m_Points == 2)
+				{
+					m_Texture.texture = m_Round2Textures[index];
+				}
+				else
+				{
+					m_Texture.texture = m_Round3Textures[index];
+				}
+				m_Timer = 1.0f;
+			}
+			m_Texture.enabled = true;
 		}
 	}
 }
