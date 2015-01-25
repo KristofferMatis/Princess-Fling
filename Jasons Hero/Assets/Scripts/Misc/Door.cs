e2299bdfa5b823ee3c590d4fb0caf282bf0dc08d
unknown <hein0040@WJ316-12.ottawa.ad.algonquincollege.com>
2015-01-24 21:13:24 -0500

    Added doors.

---
+++ Jasons Hero/Assets/Scripts/Misc/Door.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	public Transform m_Goto;
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.name == "Princess")
+		{
+			other.transform.position = m_Goto.position;
+		}
+	}
+}
